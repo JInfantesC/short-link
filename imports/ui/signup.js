9a3e5588d0e8e723e0fe0774094c9b937354b38a
JInfantesC <jose.infantes@protonmail.com>
2018-03-01 22:57:01 +0100

    Estado del componente

--- imports/ui/signup.js
+++ imports/ui/signup.js
@@ -2,11 +2,26 @@ import React from "react";
 import {Link} from "react-router";
 
 export default class Signup extends React.Component{
+    constructor(props){
+        super(props);//Estamos sobrecargando el constructor. Por eso ejecutamos super. Solo podemos usar super en el constructor
+        this.state={
+            error:""
+        }
+    }
+    onSubmit(e){
+        e.preventDefault();
+        this.setState({error:"Algo ocurrió"})
+    }
     render(){
         return (
             <div>
                 <h1>Registro en Short-Link</h1>
-                <p>Formulario de registro</p>
+                {this.state.error?<p>{this.state.error}</p>:undefined}
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="email" name="email" placeholder="e-mail"/>
+                    <input type="password" name="password" placeholder="Contraseña"/>
+                    <button>Crear cuenta</button>
+                </form>
                 <Link to="/">Volver a login</Link>
             </div>
         )
