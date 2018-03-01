dad7b83748cdd754c398ebaf5ebe160710254e9f
JInfantesC <jose.infantes@protonmail.com>
2018-03-01 20:49:28 +0100

    Botón history.push

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,7 +1,18 @@
 import React from "react";
+import {browserHistory} from "react-router";
 
 export default class Link extends React.Component{
-    render(){
-        return (<p>Link Component</p>)
+    onLogout(){
+        browserHistory.push("/")
     }
+    render(){
+        return (
+            <div>
+                <h1>Componente Link</h1>
+                <p>Contenido del componente Link</p>
+                <button onClick={this.onLogout.bind(this)}>Cerrar sesión</button>
+            </div>
+        )
+    }
+
 }
