89a8f0bd8e077402434de03f5303d71a999d4ecd
JInfantesC <jose.infantes@protonmail.com>
2018-03-04 14:39:18 +0100

    Refactorizar usuarios

---
+++ imports/api/users.js
@@ -0,0 +1,41 @@
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema from "simpl-schema";
+import {Accounts} from "meteor/accounts-base";
+
+Accounts.validateNewUser((user)=>{
+    const email=user.emails[0].address;
+    try {
+        new SimpleSchema({
+            email:{
+                type:String,
+                regEx:SimpleSchema.RegEx.Email
+            }
+        }).validate({email})
+    }catch(e){
+        throw new Meteor.Error(400, e.message);
+    }
+    return true;
+});
+/*Simple schema demo
+const employeeSchema=new SimpleSchema({
+    name: {
+        type:String,
+        min:1,
+        max:200
+    },
+    hourlyWage: {
+        type:Number,
+        min:0,
+        optional:true
+    },
+    email: {
+        type:String,
+        regEx:SimpleSchema.RegEx.Email,
+        optional:true
+    }
+});
+employeeSchema.validate({
+    name:"aaaa",
+    hourlyWage:11,
+    email:"hello@mail.com"
+});*/
--- server/main.js
+++ server/main.js
@@ -1,43 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-import SimpleSchema from "simpl-schema";
-import {Accounts} from "meteor/accounts-base";
+import "../imports/api/users";  //No exportamos nada. Solamente se va a ejecutar la página.
 
 Meteor.startup(() => {
-    Accounts.validateNewUser((user)=>{
-        const email=user.emails[0].address;
-        try {
-            new SimpleSchema({
-                email:{
-                    type:String,
-                    regEx:SimpleSchema.RegEx.Email
-                }
-            }).validate({email})
-        }catch(e){
-            throw new Meteor.Error(400, e.message);
-        }
-        return true;
-    });
-    /*Simple schema demo
-    const employeeSchema=new SimpleSchema({
-        name: {
-            type:String,
-            min:1,
-            max:200
-        },
-        hourlyWage: {
-            type:Number,
-            min:0,
-            optional:true
-        },
-        email: {
-            type:String,
-            regEx:SimpleSchema.RegEx.Email,
-            optional:true
-        }
-    });
-    employeeSchema.validate({
-        name:"aaaa",
-        hourlyWage:11,
-        email:"hello@mail.com"
-    });*/
+
 });
