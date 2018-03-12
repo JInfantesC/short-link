e7ab8470cff929ad5e147d81269bf8305ae2f57c
JInfantesC <jose.infantes@protonmail.com>
2018-03-12 21:00:01 +0100

    Middleware y redirigir

--- server/main.js
+++ server/main.js
@@ -1,9 +1,21 @@
 import { Meteor } from 'meteor/meteor';
+import {WebApp} from "meteor/webapp";
+
 import "../imports/api/users";  //No exportamos nada. Solamente se va a ejecutar la página.
-import "../imports/api/links";
+import {Links} from "../imports/api/links";
 
 import "../imports/startup/simple-schema-configuration";
 
 Meteor.startup(() => {
-
+    WebApp.connectHandlers.use((request, response, next)=>{//Middleware function
+        const _id=request.url.slice(1);
+        const link=Links.findOne({_id});//Selecciona un único elemento y devuelve el objeto.
+        if (link){
+            response.statusCode=302;    //Modifica el código del estado de la página
+            response.setHeader("Location",link.url);//Cambia donde está la aplicación.
+            response.end(); //Finaliza la respuesta
+        }else{
+            next();//Permite al servidor continuar
+        }
+    });
 });
