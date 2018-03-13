a727b669c2c93f8ddcc6bd800a8eafa95b2eec40
JInfantesC <jose.infantes@protonmail.com>
2018-03-13 23:01:14 +0100

    Copiar al portapapeles

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -4,12 +4,26 @@ import PropTypes from "prop-types";
 
 import {Links} from "../api/links";
 
+import Clipboard from "clipboard";
+
 
 export default class LinksListItem extends React.Component{
+    componentDidMount(){
+        this.clipboard=new Clipboard(this.refs.copy);
+        clipboard.on("success",()=>{
+
+        }).on("error",()=>{
+
+        });
+    }
+    componentWillUnmount(){
+        this.clipboard.destroy();
+    }
     render(){
         return (
         <div>
             <p>{this.props.url} <small>{this.props.shortUrl}</small></p>
+            <button ref="copy" data-clipboard-text={this.props.shortUrl}>Copiar</button>
         </div>
     )};
 }
